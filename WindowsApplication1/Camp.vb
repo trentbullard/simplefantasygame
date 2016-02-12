a6a206e4557ed25e705a28344af47165740ed587
p4ndepravity <trent.bullard@gmail.com>
2016-02-11 20:28:30 -0600

    add camp and tavernstate;

---
+++ WindowsApplication1/Camp.vb
@@ -0,0 +1,42 @@
+﻿Public Class Camp
+    Private campid As Integer
+    Private campSpecies As String
+    Private campMinLevel As Integer
+    Private campMaxLevel As Integer
+
+    Public Property id As Integer
+        Get
+            Return campid
+        End Get
+        Set(value As Integer)
+            campid = value
+        End Set
+    End Property
+
+    Public Property species As String
+        Get
+            Return campSpecies
+        End Get
+        Set(value As String)
+            campSpecies = value
+        End Set
+    End Property
+
+    Public Property minLevel As Integer
+        Get
+            Return campMinLevel
+        End Get
+        Set(value As Integer)
+            campMinLevel = value
+        End Set
+    End Property
+
+    Public Property maxLevel As Integer
+        Get
+            Return campMaxLevel
+        End Get
+        Set(value As Integer)
+            campMaxLevel = value
+        End Set
+    End Property
+End Class
---
+++ WindowsApplication1/TavernState.vb
@@ -0,0 +1,81 @@
+﻿Imports WindowsApplication1
+
+Public Class TavernState
+    Private tavernStateid As Integer
+    Private tavernStatePlayerState As PlayerState
+    Private tavernStateHires(4) As Creature
+    Private tavernStateQuests(4) As Quest
+
+    Public Sub New()
+        tavernStateid = -1
+        tavernStatePlayerState = New PlayerState
+        tavernStateHires = Nothing
+        tavernStateQuests = Nothing
+    End Sub
+
+    Public Sub New(id As Integer)
+        tavernStateid = id
+        tavernStatePlayerState = New PlayerState
+        tavernStateHires = Nothing
+        tavernStateQuests = Nothing
+    End Sub
+
+    Public Sub New(state As PlayerState)
+        tavernStateid = -1
+        tavernStatePlayerState = state
+        tavernStateHires = Nothing
+        tavernStateQuests = Nothing
+    End Sub
+
+    Public Sub New(row As GameDatabaseDataSet.TavernStatesRow, creatures As Creature(), quests As Quest())
+        tavernStateid = row("id")
+        If row("playerStateid") = currentState.id Then
+            tavernStatePlayerState = currentState
+        Else
+            Dim stateid As Integer = row("playerStateid")
+            tavernStatePlayerState = New PlayerState(stateid)
+        End If
+        tavernStateHires = creatures
+        tavernStateQuests = quests
+    End Sub
+
+    Public Sub New(id As Integer, state As PlayerState, creatures As Creature(), quests As Quest())
+        tavernStateid = id
+        tavernStatePlayerState = state
+        tavernStateHires = creatures
+        tavernStateQuests = quests
+    End Sub
+
+    Public ReadOnly Property id As Integer
+        Get
+            Return tavernStateid
+        End Get
+    End Property
+
+    Public Property playerState As PlayerState
+        Get
+            Return tavernStatePlayerState
+        End Get
+        Set(value As PlayerState)
+            tavernStatePlayerState = value
+        End Set
+    End Property
+
+    Public Property hires As Creature()
+        Get
+            Return tavernStateHires
+        End Get
+        Set(value As Creature())
+            tavernStateHires = value
+        End Set
+    End Property
+
+    Public Property quests As Quest()
+        Get
+            Return tavernStateQuests
+        End Get
+        Set(value As Quest())
+            tavernStateQuests = value
+        End Set
+    End Property
+End Class
